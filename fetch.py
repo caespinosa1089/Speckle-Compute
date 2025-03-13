acb468e1cde0a1723e8f8d18e8d50a7980001f62
caespinosa1089 <80283387+caespinosa1089@users.noreply.github.com>
2025-03-13 18:23:16 +0100

    NodeGenApp
    Test

--- fetch.py
+++ fetch.py
@@ -7,9 +7,9 @@ def send_to_compute():
     #Credentials
     compute_rhino3d.Util.url = "http://3.78.144.62/"
     compute_rhino3d.Util.apiKey = "SpeckleCompute02"
-    send_to_compute_stream = "https://macad.speckle.xyz/streams/26d9a49958"
-    receive_from_compute_stream = "https://macad.speckle.xyz/streams/a63c4e60ed"
-    gh_definition = "example.ghx"
+    send_to_compute_stream = "https://macad.speckle.xyz/streams/3a0c9390fa"
+    receive_from_compute_stream = "https://macad.speckle.xyz/streams/ba6f903648"
+    gh_definition = "NodeGen.ghx"
 
     gh_data = open(gh_definition, mode="r", encoding="utf-8-sig").read()
     data_bytes = gh_data.encode("utf-8")
